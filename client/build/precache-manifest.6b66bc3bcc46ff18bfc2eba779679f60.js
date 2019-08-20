12f7fcf4ce0e48d485780c89ea5687f3fdc6e99d
Perluceo <FullStackDevSMU@gmail.com>
2019-08-20 14:41:23 -0500

    hero

---
+++ client/build/precache-manifest.6b66bc3bcc46ff18bfc2eba779679f60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09b6c378b43df717771da55394a207fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f556c3bf1731cdd34bf2",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "437bd8fc8c69d7e64119",
+    "url": "/static/js/2.88343a08.chunk.js"
+  },
+  {
+    "revision": "f556c3bf1731cdd34bf2",
+    "url": "/static/js/main.37f88784.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  }
+]);
